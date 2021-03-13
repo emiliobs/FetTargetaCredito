2ca16d48c90b0a1ff044662325166b070efa608c
Emilio Barrea <barrera_emilio@yahoo.es>
2021-03-13 17:29:27 +0000

    Configuración Google Fonts  - Listado de Tarjetas

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjeta-credito.component.css']
 })
 export class TarjetaCreditoComponent implements OnInit {
+//Variables:
+listarTarjetas: any[] = [
+  { titular: "Emilio Barrera", numeroTarjeta: "5454454545544545", fechaExpiracion : "05/05", cvv : "125" },
+  { titular: "Lina Marulanda", numeroTarjeta: "8788778788878758", fechaExpiracion : "11/12", cvv : "452" }
+];
 
   constructor() { }
 
